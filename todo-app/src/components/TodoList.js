6f14ecb7256481afc5a4551a97b85e64ad707d1a
haniey <h.rastifar@mail.sbu.ac.ir>
2022-03-10 09:23:05 -0800

    'modification'

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -3,53 +3,26 @@ import { Col, Container, Row } from "react-bootstrap";
 import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: this.props.todos,
-    };
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.todos !== this.props.todos) {
-      this.setState({
-        list: this.props.todos,
-      });
-    }
-  }
   deleteHandler = (item) => {
-    console.log("delete in todo list ",item);
+    console.log("delete in todo list ", item);
     this.props.handleTodosInRemove(item);
-    const updatedList = this.state.list.filter((todo) => todo.id !== item.id);
-    this.setState({ list: updatedList });
-    console.log("todolist dl ",this.state.list);
   };
   editHandler = (item) => {
     console.log("edit in todo List :", item);
-    this.props.handleTodosInEdit(item);   
-    this.setState((prevState) => ({
-      list: prevState.list.map((obj) =>
-        obj.id === item.id ? Object.assign(obj, { value: item.value }) : obj
-      ),
-    }));
-    console.log("todolist edit ",this.state.list);
-    // const updatedList = this.state.list.filter(todo => todo.id !== item.id)
-
-    // // const updatedList = list.filter(item => item.id !== id);
-    // // this.props.todos = updatedList
-    // this.setState({ list: updatedList });
+    this.props.handleTodosInEdit(item);
   };
   render() {
-    const { list } = this.state;
+    const { todos } = this.props;
     return (
       <Container>
         <Row>
           <Col className="mx-auto" lg={10}>
-            {list.map((item) => (
+            {todos.map((item) => (
               <TodoItem
                 key={item.id}
                 id={item.id}
                 title={item.value}
-                remove={()=>this.deleteHandler(item)}
+                remove={() => this.deleteHandler(item)}
                 edit={this.editHandler}
               />
             ))}
